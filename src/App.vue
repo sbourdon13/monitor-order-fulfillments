a6405dda76a1018f48c924192c857580b97b7814
sbourdon13 <sbourdon13@hotmail.fr>
2019-05-06 12:53:51 +0200

    display status as a badge & make operatur and status colomns sortable

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,13 @@
   <div id="app">
     <h1>Monitor your orders</h1>
     <!-- <b-table striped hover :items="orders"></b-table> -->
-    <b-table striped hover :items="items" :fields="fields">
+    <b-table striped hover responsive :items="items" :fields="fields">
+      <!-- Display badges for the status column -->
+      <template slot="status" slot-scope="row">
+        <b-badge :variant="badgeColors[row.item.status]">{{ row.item.statusLabel }}</b-badge>
+      </template>
+
+      <!-- details of previous order events of the same reference -->
       <template slot="show_details" slot-scope="row">
         <b-button
           size="sm"
@@ -22,7 +28,7 @@
               <b>{{ event.time.toLocaleString("en-GB") }}</b>
             </b-col>
             <b-col>{{ event.description }}</b-col>
-            <b-col>{{ event.status }}</b-col>
+            <b-col>{{ event.statusLabel }}</b-col>
           </b-row>
         </b-card>
       </template>
@@ -40,7 +46,32 @@ export default {
       ordersByReference: {}, // keys are the references, values are arrays of all order events of this reference
       orderEvents: [], // all order events
       items: [], // orders to be displayed (one item per reference),
-      fields: ["reference", "operator", "description", "status", "show_details"]
+      fields: [
+          { key: 'reference', sortable: false },
+          { key: 'operator', sortable: true },
+          { key: 'description', sortable: false },
+          { key: 'status', sortable: true },
+          { key: 'show_details', sortable: false }
+        ],
+      status: {
+        CREATED: "CREATED",
+        TRANSMITTED: "TRANSMITTED",
+        IN_PREPARATION: "IN PREPARATION",
+        PREPARED: "PREPARED",
+        SHIPPED: "SHIPPED",
+        DELIVERY_EXCEPTION: "DELIVERY EXCEPTION",
+        DELIVERED: "DELIVERED"
+      },
+      badgeColors: {
+        CREATED: "primary",
+        TRANSMITTED: "secondary",
+        IN_PREPARATION: "warning",
+        PREPARED: "info",
+        SHIPPED: "dark",
+        DELIVERY_EXCEPTION: "danger",
+        DELIVERED: "success",
+        UPDATE_DATA: "light"
+      }
     };
   },
   created() {
@@ -72,18 +103,20 @@ export default {
       );
     },
     getOrderData(data) {
-      let status, description;
+      let status = data.payload.short;
+      let statusLabel, description;
       const reference = data.payload.reference;
       const time = new Date(Date.parse(data.create_time));
 
       if (data.payload.subtype === "status_update") {
         description = data.payload.description;
-        status = data.payload.short;
+        statusLabel = this.status[data.payload.short];
       } else {
         description = `Update ${data.payload.short}: ${
           data.payload.description
         }`;
-        status = "Updating data";
+        status = "UPDATE_DATA";
+        statusLabel = "Updating data";
       }
 
       let order = {
@@ -91,13 +124,14 @@ export default {
         time,
         reference,
         description,
-        status
+        status,
+        statusLabel
       };
 
       this.orderEvents.push(order);
 
       if (this.ordersByReference[reference]) {
-        this.ordersByReference[data.payload.reference].push(order);
+        this.ordersByReference[reference].push(order);
         let index = this.items.findIndex(item => item.reference === reference);
         this.items[index] = order;
       } else {
